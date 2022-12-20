eee095aa4f1d7913d68296dc9d5e2324bba6f150
TechnoDotΩ <73961295+TechDudie@users.noreply.github.com>
2022-12-19 20:19:58 -0600

    Delete src directory

--- src/update.py
+++
@@ -1,6 +0,0 @@
-from .event import *
-
-def update():
-    for structure in registered_structures:
-        if structure.location != (-1, -1):
-            structure.spawn(structure.location[0], structure.location[1])
