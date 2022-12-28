cf2a7e31776647e20bfb2cfaf644a7027db6cae9
TechnoDotΩ <73961295+TechDudie@users.noreply.github.com>
2022-12-27 22:11:27 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,28 +4,32 @@ from src.tile import *
 from src.map import *
 from src.item import *
 from src.event import *
+from src.craft import *
 
 import src.planets.earth as earth
 
-import src.structures.tree as tree
-
 root = tk.Tk()
 
-root.geometry("1280x640+100+100")
+width = 1280
+height = 640
+root.geometry(f"{width}x{height}+100+100")
 root.title("Revolution")
 
-canvas = tk.Canvas(root, width=1280, height=640)
-canvas.pack()
+canvas = tk.Canvas(root, width=width, height=height, highlightthickness=0)
+canvas.place(x=0, y=0)
 
-map = Map(canvas)
+map = Map(canvas, width, height)
 earth.generator(map, canvas)
+
+craft = CraftFrame(root, int(width / 4), int(height / 4), int(width / 2), int(height / 2))
 
 setupCanvas(canvas)
 setupMap(map)
+setupCraft(craft)
 setupHUD()
 
 canvas.bind("<Button-1>", lclick)
 canvas.bind("<Button-3>", rclick)
-canvas.bind("<Key>", key)
+root.bind("<KeyPress>", key)
 
 root.mainloop()
